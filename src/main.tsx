b52dae986484e1fe2a4dece3bef0242960c572e8
eduardochamp1 <zezouain@gmail.com>
2023-04-09 00:38:04 -0300

    .

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 
-
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-
 //Pages
 import Home from './routes/Home';
 
